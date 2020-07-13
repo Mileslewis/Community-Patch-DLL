35fc1ad6cf7340c91bce5ee98b364d02b4fa999d
RecursiveVision <66801010+RecursiveVision@users.noreply.github.com>
2020-07-13 11:15:17 -0400

    New DoTurn function

--- CvGameCoreDLL_Expansion2/CvDiplomacyAI.cpp
+++ CvGameCoreDLL_Expansion2/CvDiplomacyAI.cpp
@@ -9465,145 +9465,162 @@ void CvDiplomacyAI::SetCSBullyTargetPlayer(PlayerTypes ePlayer)
 //	-----------------------------------------------------------------------------------------------
 
 
-
 // ************************************
 // Turn Stuff
 // ************************************
 
 
-
 /// Runs every turn!  The order matters for a lot of this stuff, so be VERY careful about moving anything around (!)
+/// Recursive: Modified the order of things here substantially. The AI-only check in this function has been replaced by AI-only checks in all the functions called for additional flexibility.
 void CvDiplomacyAI::DoTurn(DiplomacyPlayerType eTargetPlayer)
 {
+	// Function start
 	m_eTargetPlayer = eTargetPlayer;
-	// Military Stuff
+	////////////////////
+
+	// War damage decay and war state evaluation - comes before everything else!
 	DoWarDamageDecay();
-	DoUpdateWarDamageLevel();
+	DoUpdateWarDamageLevels();
+	DoEstimateOtherPlayerWarDamageLevels();
+	DoUpdateWarStates();
+
+	// First evaluation of war and military statistics! We will evaluate again after changing our Opinion, Approach & War Status below.
+	// Update strength assessments
 	DoUpdatePlayerMilitaryStrengths();
 	DoUpdatePlayerEconomicStrengths();
-
-	DoUpdateWarmongerThreats();
-	DoUpdateMilitaryThreats();
 	DoUpdatePlayerTargetValues();
-	DoUpdateWarStates();
+
+	// Update threat levels
+	DoUpdateMilitaryThreats();
+	DoUpdateWarmongerThreats();
+	DoUpdateWarProjections();
+
+	// First evaluation of peace willingness
 	DoUpdateWarProjections();
 	DoUpdateWarGoals();
-
 	DoUpdatePeaceTreatyWillingness();
 
-	// Issues of Dispute
-	DoUpdateLandDisputeLevels();
-	DoUpdateVictoryDisputeLevels();
-#if defined(MOD_BALANCE_CORE_DIPLOMACY)
-	if(MOD_BALANCE_CORE_DIPLOMACY)
-	{
-		DoUpdateVictoryBlockLevels();
-	}
-#endif
-	DoUpdateWonderDisputeLevels();
-	DoUpdateMinorCivDisputeLevels();
+	// Forget about diplomatic penalties that have expired
+	DoForgetExpiredPenalties();
 
-	// What we think other Players are up to
-#if defined(MOD_BALANCE_CORE)
-	DoUpdateOtherPlayerWarDamageLevel();
-#endif
-	DoUpdateEstimateOtherPlayerLandDisputeLevels();
-	DoUpdateEstimateOtherPlayerVictoryDisputeLevels();
-	//DoUpdateEstimateOtherPlayerVictoryBlockLevels();
-	DoUpdateEstimateOtherPlayerMilitaryThreats();
-
-	// Look at the situation
+	// Update aggressive postures
 	DoUpdateMilitaryAggressivePostures();
 	DoUpdateExpansionAggressivePostures();
 	DoUpdatePlotBuyingAggressivePostures();
-	DoTestPromises(); // Has any player gone back on any promises he made?
-	DoTestUntrustworthyFriends(); // Do we consider any players backstabbers?
-	DoUpdateEasyTargets(); // Are any players easy to attack?
-	
-	// Our guess as to other players' diplomatic stances
-	//DoUpdateOpinionTowardsUsGuesses();
-	DoUpdateApproachTowardsUsGuesses();
-	DoEstimateOtherPlayerOpinions();
-	DoEstimateOtherPlayerApproaches();
-	LogOtherPlayerGuessStatus();
-	
-	/*
-	if (!GetPlayer()->isHuman())
-	{
-		DoSendAIAggressiveMilitaryWarnings(); // tell other AIs to move their troops (may trigger war)
-		DoSendAIPromiseRequests(); // tell other AIs to stop doing things we don't like
-#if defined(MOD_DIPLOMACY_CIV4_FEATURES)
-		if (MOD_DIPLOMACY_CIV4_FEATURES)
-		{
-			DoSendAIRevokeVassalageStatement(); // tell our AI master we want independence, if we can and we want it
-		}
-#endif
-	}
-	*/
 
-	// Player Opinion & Approach
-	vector<PlayerTypes> v;
+	// Update anything relating to promises made by other players
+	DoUpdatePromiseStatus();
+
+	// Update direct competition dispute levels
+	DoUpdateLandDisputeLevels();
+	DoUpdateWonderDisputeLevels();
+	DoUpdateMinorCivDisputeLevels();
+
+	// Update victory-based dispute levels
+	DoUpdateVictoryDisputeLevels();
+	DoUpdateVictoryBlockLevels();
+
+	// Update our estimates of what we think other players are up to
+	DoEstimateOtherPlayerMilitaryThreats();
+	DoEstimateOtherPlayerWarmongerThreats();
+	DoUpdateEstimateOtherPlayerLandDisputeLevels();
+	DoUpdateEstimateOtherPlayerWonderDisputeLevels();
+	DoUpdateEstimateOtherPlayerMinorCivDisputeLevels();
+	DoUpdateEstimateOtherPlayerVictoryDisputeLevels();
+	DoUpdateEstimateOtherPlayerVictoryBlockLevels();
+
+	// Update our estimates of what other players think of each other
+	// Do ourselves last, since it's the most important to us, and we'll have more information going into the evaluation
+	DoEstimateOtherPlayerOpinions();
+	DoEstimateOtherPlayerApproaches(); // AI makes share opinion requests of other AIs as part of this
+	DoUpdateOpinionTowardsUsGuesses();
+	DoUpdateApproachTowardsUsGuesses();
+
+	// Are any players now untrustworthy to us?
+	DoUpdateUntrustworthyFriends(/*bTeamCheck*/ false); // Sets to true if the individual player is untrustworthy, false otherwise
+	DoUpdateUntrustworthyFriends(/*bTeamCheck*/ true); // Sets to true if the individual player OR anyone on that player's team is untrustworthy, false otherwise
+
+	// Opinion
 	DoUpdateOpinions();
-	DoUpdateMajorCivApproaches(v, false);
+
+	// Test if any players are easy attack targets (do this after Opinion, since Opinion affects the perception of players)
+	DoUpdateEasyAttackTargets();
+
+	// Approach
+	vector<PlayerTypes> v; // Need an empty vector here, since on normal turn updates, no players are being reevaluated
+	DoUpdateMajorCivApproaches(v, /*bBetweenTurnsUpdate*/ false); // A between turns update will call DoUpdatePlannedWarsAndDemands(), but the normal turn update won't (also matters for the approach curve)
+
+	// Special Case War Stuff
+	// All of these functions may trigger WAR! and a subsequent reevaluation of opinions/approaches.
+#if defined(MOD_DIPLOMACY_CIV4_FEATURES)
+	if (MOD_DIPLOMACY_CIV4_FEATURES)
+	{
+		DoRequestIndependence(); // Request independence from our master
+	}
+#endif
+	DoProcessCoopWars(); // Handles everything involving existing coop war agreements
+	DoSendWarTauntsToAIPlayers(); // Send move troops requests and certain taunts to AI players!
+	DoSendWarTauntsToHumanPlayers(); // Send move troops requests and certain taunts to humans in singleplayer! Handled differently in multiplayer.
+
+	// Normal War Stuff
+	DoUpdatePlannedWarsAndDemands(); // Plan any other wars we want to declare, as well as demands we want to make
+	MakeWar(); // Declare war, if we're ready
+	MakeDemands(); // Make demands of other civs, if we're ready
+
+	// Second evaluation of war and military statistics - functions above may have changed it!
+	// bUpdateLogsSpecial is set to true for all of these functions - don't update the logs if no change happened
+	DoUpdateWarDamageLevels(true);
+	DoEstimateOtherPlayerWarDamageLevels(true);
+	DoUpdateWarStates(true);
+	DoUpdatePlayerMilitaryStrengths(true);
+	DoUpdatePlayerEconomicStrengths(true);
+	DoUpdatePlayerTargetValues(true);
+	DoUpdateMilitaryThreats(true); // DoUpdateWarmongerThreats is updated whenever warmongering penalties change, so don't update that here
+	DoUpdateWarProjections(true);
+
+	// Minor Civ Approach
 	DoUpdateMinorCivApproaches();
 
-	// These functions actually DO things, and we don't want the shadow AI behind a human player doing things for him
-	if(!GetPlayer()->isHuman())
+	// Second evaluation of peace willingness
+	DoUpdateWarGoals(true);
+	DoUpdatePeaceTreatyWillingness(true);
+
+	// Make peace with humans we're at war with (humans are usually the biggest threat, so this is an exception to the "contact AIs first" rule).
+	DoMakePeaceWithHumanPlayers();
+
+	// Make peace with AI major civs we're at war with
+	DoMakePeaceWithAIPlayers();
+
+	// Make peace with City-States we're at war with
+	DoMakePeaceWithMinors();
+
+	// Update our valuation of other players as friends and enemies
+	DoRelationshipPairing();
+	DoUpdateDefensivePactValuation(); // DP valuation uses Friend/Competitor values from DoRelationshipPairing(), so it needs to be updated separately
+
+	// Planning Exchanges - Update our desire for Declarations of Friendship, Defensive Pacts and Research Agreements
+	DoUpdatePlanningExchanges();
+
+#if defined(MOD_DIPLOMACY_CIV4_FEATURES)
+	// Select the tax rate for our vassals, if we can
+	if (MOD_DIPLOMACY_CIV4_FEATURES)
 	{
-#if defined(MOD_DIPLOMACY_CIV4_FEATURES)
-		if(MOD_DIPLOMACY_CIV4_FEATURES)
-		{
-			DoDetermineTaxRateForVassals();
-		}
-#endif
-#if defined(MOD_BALANCE_CORE)
-		DoUpdateDemands();
-#endif
-		MakeWar();
-		DoMakePeaceWithMinors();
-#if defined(MOD_DIPLOMACY_CIV4_FEATURES)
-		if (MOD_DIPLOMACY_CIV4_FEATURES)
-		{
-			DoMakePeaceWithVassals();
-		}
-#endif
-#if defined(MOD_BALANCE_CORE)
-		DoRelationshipPairing();
-#endif
-		DoUpdatePlanningExchanges();
-		DoContactMinorCivs();
-		DoContactMajorCivs();
-#if defined(MOD_ACTIVE_DIPLOMACY)
-		if(GC.getGame().isReallyNetworkMultiPlayer() && MOD_ACTIVE_DIPLOMACY)
-		{
-			GC.getGame().GetGameDeals().DoCancelAllProposedMPDealsWithPlayer(GetPlayer()->GetID(), DIPLO_AI_PLAYERS);
-		}
-		else
-		{
-			GC.getGame().GetGameDeals().DoCancelAllProposedDealsWithPlayer(GetPlayer()->GetID());	//Proposed deals with AI players are purely transitional.
-		}
-#else
-		GC.getGame().GetGameDeals().DoCancelAllProposedDealsWithPlayer(GetPlayer()->GetID());	//Proposed deals with AI players are purely transitional.
-#endif
-	}
-/*
-#if defined(MOD_DIPLOMACY_CIV4_FEATURES)
-	if(MOD_DIPLOMACY_CIV4_FEATURES)
-	{
-		DoUpdateGlobalStates();
+		DoDetermineTaxRateForVassals();
 	}
 #endif
-*/
-	// Update Counters
-	DoCounters();
 
-	// Save our current war value estimates, so we can compare them with the updated estimates next turn
-	CacheOtherPlayerWarValuesLost();
+	// Any minor civs we want to chat with?
+	DoContactMinorCivs();
 
-	LogStatus();
-	LogWarStatus();
-	LogStatements();
+	// Any major civs we want to chat with?
+	DoContactAIPlayers(); // AI players are contacted FIRST, and humans LAST! If the AI wants a trade item (including vassalage) from a human, this is recorded for DoContactHumanPlayers (to make sure AI-to-AI trading doesn't deprive the human of all opportunities).
+	DoContactHumanPlayers(); // As part of DoContactHumanPlayers, if the human refuses the trade, AI may then attempt to trade for the item with other AI players before resuming discussion with the human.
 
+	// Logging
+	LogTurnStatus();
+
+	////////////////////
+	// Function end
 	m_eTargetPlayer = DIPLO_ALL_PLAYERS;
 }
 
